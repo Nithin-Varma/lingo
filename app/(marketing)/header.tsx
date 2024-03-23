ec9b5a2c268d5db627ee854b8228d5ace6244917
Nithin-Varma <mnithin1422@gmail.com>
2024-03-23 23:23:39 +0530

    ..

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -2,7 +2,7 @@ import { ClerkLoading } from "@clerk/nextjs"
 import { Loader } from "lucide-react"
 import Image from "next/image"
 export const Header = () => {
-    return(
+    return (
         <header className="h-20 w-full border-b-2 border-slate-200 px-4">
             <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full bg-red-500">
                 <div className="pt-8 pl-4 pb-7 items-center gap-x-3">
